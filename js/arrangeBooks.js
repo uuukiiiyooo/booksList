0551105da21263986ddb6db96e2049b2ce093267
uuukiiiyooo <uuukiiiyooo@gmail.com>
2023-03-21 09:50:52 -0600

    Add lists to collected data

--- js/arrangeBooks.js
+++ js/arrangeBooks.js
@@ -1,22 +1,53 @@
-const bookListContainer = document.getElementById('bookList');
-const submitBook = document.querySelector('#submitBook');
-let booksDetails = [];
+const bookList = document.getElementById('bookList');
+const submitButton = document.getElementById('submitButton')
 
-submitBook.addEventListener('click', () => {
-  const singleBook = document.getElementById('bookTitle').value;
-  const singleAuthor = document.getElementById('bookAuthor').value;
-  let newBook = new DisplayBooks(singleBook, singleAuthor);
-  booksDetails.push(newBook);
-  booksDetails.forEach((book) => {
-    bookListContainer.innerHTML += `
-      <p>${book.title}<br>${book.author}</p>
-      <button>Remove</button>
-    `
-});
-})
+let bookShelf = JSON.parse(localStorage.getItem('bookShelf')) || [];
 
-function DisplayBooks(title, author) {
-  this.title = title;
-  this.author = author;
+function addBook(title, author) {
+  const newBook = {
+    title: title,
+    author: author,
+  };
+
+  bookShelf.push(newBook);
+
+  localStorage.setItem('bookShelf', JSON.stringify(bookShelf));
+  document.getElementById('bookTitle').value = '';
+  document.getElementById('bookAuthor').value = '';
+
+  displayBook(newBook);
 }
-// const example = new DisplayBooks('Book name', 'Book Author')
+
+function displayBook(book) {
+  const singleBook = document.createElement('li');
+  singleBook.setAttribute('id', `${book.title}-${book.author}`);
+
+  const bookPrint = document.createElement("span");
+  bookPrint.innerHTML = `${book.title}<br>${book.author}<br>`;
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Leave team';
+  removeButton.addEventListener('click', () => removeBook(book.title, book.author));
+
+  singleBook.appendChild(bookPrint);
+  singleBook.appendChild(removeButton);
+  bookList.appendChild(singleBook);
+}
+
+function removeBook(title, author) {
+  bookShelf = bookShelf.filter(book => book.title !== title || book.author !== author);
+
+  localStorage.setItem('bookShelf', JSON.stringify(bookShelf));
+
+  const singleBook = document.getElementById(`${title}-${author}`);
+  if (singleBook) {
+    bookList.removeChild(singleBook);
+  }
+}
+
+bookShelf.forEach(book => displayBook(book));
+
+submitButton.addEventListener('click', () => {
+  const title = document.getElementById('bookTitle').value;
+  const author = document.getElementById('bookAuthor').value;
+  addBook(title, author);
+});
